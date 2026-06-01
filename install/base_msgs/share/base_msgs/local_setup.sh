d3dec48ea9c0b2b86e3054e17aed0ff28fecdf45
wu-G28 <2951482206@qq.com>
2026-06-01 09:35:37 +0800

    project colcon 0.4

--- install/base_msgs/share/base_msgs/local_setup.sh
+++ install/base_msgs/share/base_msgs/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/pi/rosdasai/ROS-2-Robot-Simulation-Competition/install/base_msgs"}
+: ${AMENT_CURRENT_PREFIX:="/home/pi/ROS-2-Robot-Simulation-Competition/install/base_msgs"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
---
+++ src/planning/src/global_planner/global_planner_normal/global_planner_normal.h
@@ -0,0 +1,13 @@
+#ifndef GLOBAL_PLANNER_NORMAL_H
+#define GLOBAL_PLANNER_NORMAL_H
+
+#include "global_planner_base.h"
+namespace planning
+{
+	class GlobalPlannerNormal : public GlobalPlannerBase
+	{
+
+	}
+}
+
+#endif // GLOBAL_PLANNER_NORMAL_H
